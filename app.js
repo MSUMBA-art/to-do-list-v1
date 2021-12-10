e6d10a46247ff2c67540da03a40b11543cbbe8b5
ALEXANDER <msumba83@gmail.com>
2021-12-10 22:37:53 +0200

    done css

--- app.js
+++ app.js
@@ -1,42 +1,35 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
-let items = ["Buy Food", "Cook Food", "Eat Food"] ;
+let items = ["Buy Food", "Cook Food", "Eat Food"];
 
-app.set('view engine', 'ejs'); // always below express();
+app.set("view engine", "ejs"); // always below express();
 
-app.use(bodyParser.urlencoded( {extended: true})) //for app.post to be triggered
-app.use(express.static("public"))
+app.use(bodyParser.urlencoded({ extended: true })); //for app.post to be triggered
+app.use(express.static("public"));
 
-app.get('/', function(req, res)  {
-    let today = new Date();
-   
-    let options = {
-      weekday: "long",
-      day: "numeric",
-      month: "long"
-    };
+app.get("/", function (req, res) {
+  let today = new Date();
 
-   
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
 
-    let day = today.toLocaleDateString("en-US", options); // Saturday, September 17, 2016
+  let day = today.toLocaleDateString("en-US", options); // Saturday, September 17, 2016
 
-     res.render('list', {kindOfDay: day, newListItems: items});
-
-       
+  res.render("list", { kindOfDay: day, newListItems: items });
 });
 
 app.post("/", function (req, res) {
-        let item = req.body.newItem;
+  let item = req.body.newItem;
 
-        items.push(item)
-         res.redirect("/");
+  items.push(item);
+  res.redirect("/");
+});
 
-    })
-
-
-
-app.listen( process.env.PORT || 3000, () => {
-  console.log(`Example app listening`)
-})
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Example app listening`);
+});
